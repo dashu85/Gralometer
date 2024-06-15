a0c73c2320605cd5581ea0705132e8a51bf21d15
Marcus Benoit <benoit.marcus@gmail.com>
2024-06-15 11:10:09 +0200

    Initial Commit

---
+++ Gralometer/GralometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GralometerApp.swift
+//  Gralometer
+//
+//  Created by Marcus Benoit on 15.06.24.
+//
+
+import SwiftUI
+
+@main
+struct GralometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
