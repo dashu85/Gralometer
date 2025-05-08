67d31fd296a336938e201f1f652753a1d7745e18
Marcus Benoit <marcus.benoit@icloud.com>
2025-05-08 21:10:19 +0200

    Updated ProfileView to upload image and display it immediately. Added DetailView for Profile picture with pinch and zoom and dragging capabilities.

---
+++ Core/Profile/ProfilePictureDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePictureDetailView.swift
+//  Gralometer
+//
+//  Created by Marcus Benoit on 08.05.25.
+//
+
+import SwiftUI
+
+struct ProfilePictureDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePictureDetailView()
+}
